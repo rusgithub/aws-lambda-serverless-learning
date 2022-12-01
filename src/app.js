009c6717dc0b6b21ccb316963e5f22a5be935fb9
ruslan <ruslanusd@gmail.com>
2022-12-01 15:27:18 +0100

    fix to make sls offline work

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get('/api/info', (req, res) => {
@@ -9,5 +10,5 @@ app.get('/api/info', (req, res) => {
 app.post('/api/v1/getback', (req, res) => {
   res.send({ ...req.body });
 });
-//app.listen(3000, () => console.log(`Listening on: 3000`));
+app.listen(3000, () => console.log(`Listening on: 3000`));
 module.exports.handler = serverless(app);
